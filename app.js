79f33ce34543a29606fe561fe1a99f87a15126f1
Zaid <ztawalbeh8@gmail.com>
2024-07-19 15:48:42 +0300

    fix the link whit js file

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+
 const form = document.getElementById("T-form");
 const table = document.getElementById("tab");
-const tbody = getElementsByTagName("tbody")[0];
-
-
+const tbody = document.getElementsByTagName("tbody")[0];
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    let product = e.getElementById("productName").value;
-    let price = e.getElementById("productPrice").value;
-    let description = e.getElementById("productDescription").value;
+    let product = document.getElementById("productName").value;
+    let price = document.getElementById("productPrice").value;
+    let description = document.getElementById("productDescription").value;
 
     const newTr = table.insertRow();
     const pNameCell = newTr.insertCell(0);
@@ -15,7 +14,7 @@ form.addEventListener("submit",(e)=>{
     const pDescrCell = newTr.insertCell(2);
 
     tbody.appendChild(newTr);
-    newTr.appendChild(pNameCell,pPriceCell,pDescrCell);
+    newTr.appendChild(pDescrCell,pPriceCell,pNameCell);
 
     pNameCell.textContent = product ;
     pPriceCell.textContent = price;
